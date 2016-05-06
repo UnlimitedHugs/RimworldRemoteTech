2483d8dcc1e2282ac29fb7c4c454205538569545
UnlimitedHugs <symbolic@land.ru>
2016-05-07 03:05:18 +0400

    1.3.1: Added trader support - Vanilla traders now carry explosives - Added a separate category for explosives (in item form)

---
+++ Source/TraderStockInjectorDef.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace RemoteExplosives {
+	public class TraderStockInjectorDef : Def {
+		public TraderKindDef traderDef;
+		public List<StockGenerator> stockGenerators = new List<StockGenerator>();
+
+		public override void PostLoad() {
+			base.PostLoad();
+			foreach (StockGenerator current in stockGenerators) {
+				current.PostLoad();
+			}
+		}
+		
+		public override void ResolveReferences() {
+			base.ResolveReferences();
+			foreach (StockGenerator current in stockGenerators) {
+				current.ResolveReferences(traderDef);
+			}
+		}
+	}
+}
