520fcea7f9fb5c50dc62074647bcccb1c82e16ec
UnlimitedHugs <unlimited.hugs@gmail.com>
2017-06-04 16:30:28 +0400

    Added stats to mining explosives

--- Source/Comp/CompProperties_MiningExplosive.cs
+++ Source/Comp/CompProperties_MiningExplosive.cs
@@ -2,12 +2,13 @@
 
 namespace RemoteExplosives {
 	public class CompProperties_MiningExplosive : CompProperties_Explosive {
-		public float rockChunkChance = .5f;
 		public float miningRadius = 2f;
-		public float resourceBreakingYield = .9f;
-		public float woodBreakingYield = .5f;
 		public float breakingPower = 68400;
 		public float resourceBreakingCost = 2f;
 		public float woodBreakingCost = 2f;
+
+		public CompProperties_MiningExplosive() {
+			compClass = typeof(CompMiningExplosive);
+		}
 	}
 }
