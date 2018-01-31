40a43829fc85d65b60fc6f1940d994d16e583174
UnlimitedHugs <unlimited.hugs@gmail.com>
2018-01-31 14:27:09 +0400

    Fixed explosives not dropping their item when deconstructed

---
+++ Source/Thing/FullDeconstructionRefund.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace RemoteExplosives {
+	/// <summary>
+	/// Placed on a building to drop the full amounts of materials on deconstruction
+	/// </summary>
+	public class FullDeconstructionRefund : DefModExtension {
+	}
+}
