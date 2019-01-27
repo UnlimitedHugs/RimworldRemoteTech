31ad09f3d978e2bb9c95698497349281c79a7bae
UnlimitedHugs <unlimited.hugs@gmail.com>
2019-01-27 20:32:07 +0400

    Limited 'red button fever' mental break to colonists

---
+++ Source/AI/MentalBreakWorker_ColonistsOnly.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+using Verse.AI;
+
+namespace RemoteTech {
+	public class MentalBreakWorker_ColonistsOnly : MentalBreakWorker {
+		public override bool BreakCanOccur(Pawn pawn) {
+			return base.BreakCanOccur(pawn) && pawn.IsColonist;
+		}
+	}
+}
