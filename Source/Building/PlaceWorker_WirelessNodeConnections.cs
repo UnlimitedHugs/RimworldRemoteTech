d613bf04f79f2fd232fc6f4c178782cb746a760f
UnlimitedHugs <unlimited.hugs@gmail.com>
2020-03-20 20:35:49 +0400

    Removed no longer needed patch

--- Source/Building/PlaceWorker_WirelessNodeConnections.cs
+++ Source/Building/PlaceWorker_WirelessNodeConnections.cs
@@ -2,18 +2,20 @@
 using Verse;
 
 namespace RemoteTech {
-	public class PlaceWorker_WirelessNodeConnections : PlaceWorker, ISelectedThingPlaceWorker {
+	public class PlaceWorker_WirelessNodeConnections : PlaceWorker {
 		public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null) {
-			var nodes = CompWirelessDetonationGridNode.GetPotentialNeighborsFor(def, center, Find.CurrentMap);
-			foreach (var node in nodes) {
-				GenDraw.DrawLineBetween(center.ToVector3Shifted(), node.parent.Position.ToVector3Shifted());
-			}
-		}
-
-		public void DrawGhostForSelected(Thing thing) {
-			CompWirelessDetonationGridNode node;
-			if (thing is ThingWithComps twc && (node = twc.GetComp<CompWirelessDetonationGridNode>()) != null && node.Enabled) {
-				node.DrawNetworkLinks();
+			if (thing != null) {
+				// while existing building is selected
+				CompWirelessDetonationGridNode node;
+				if (thing is ThingWithComps twc && (node = twc.GetComp<CompWirelessDetonationGridNode>()) != null && node.Enabled) {
+					node.DrawNetworkLinks();
+				}
+			} else {
+				// preparing to build
+				var nodes = CompWirelessDetonationGridNode.GetPotentialNeighborsFor(def, center, Find.CurrentMap);
+				foreach (var node in nodes) {
+					GenDraw.DrawLineBetween(center.ToVector3Shifted(), node.parent.Position.ToVector3Shifted());
+				}
 			}
 		}
 	}
